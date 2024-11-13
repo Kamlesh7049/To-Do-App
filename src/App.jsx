63b4a6afc27ea4a74a0e0c4f81a664b297adf717
Kamlesh7049 <Kamlesh7049>
2024-11-14 00:49:50 +0530

    Crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTask } from "./todoSlice";
+import { addTask ,deleteTask,completeTask ,uncompleteTask,editTask} from "./todoSlice";
 
 const App = () => {
   const [mywork, setMywork] = useState("");
+  const [btnStatus,setBtnStatus]=useState(true);
+  const [abtnStatus,setABtnStatus]=useState(false)
+  const [myid,setMyid]=useState("");
   const dispatch = useDispatch();
   const workData = useSelector((state) => state.todo.task);
   console.log(workData);
 
   const handleSumit=()=>{
     // dispatch(addTask(mywork));//A
-    dispatch(addTask({id:Date.now(), work:mywork}))//B
+    dispatch(addTask({id:Date.now(), work:mywork,status:false}))//B
+  }
+
+  const deletework=(id)=>{
+    dispatch(deleteTask(id));
+  }
+
+  const completework=(id)=>{
+    dispatch(completeTask(id));
+    setABtnStatus(false)
+
+  }
+  const uncompletework=(id)=>{
+    dispatch(uncompleteTask(id));
+    setABtnStatus(true)
+  }
+
+  const editData=(id,work)=>{
+    setMywork(work)
+    setBtnStatus(false);
+    setMyid(id)
+  }
+
+  const editDataSave=()=>{
+    dispatch(editTask({myid,mywork}))
+    setMywork("");
+    setBtnStatus(true)
   }
 
 
@@ -21,11 +50,29 @@ const ans=workData.map((key)=>{
     <>
     <tr>
       <td>{sno}</td>
-      <td>{key.work}</td>
-      <td> Complite</td>
-      <td> UnComplite</td>
-      <td> Delete</td>
-      <td> Edit  </td>
+      <td>{key.status ?
+       <span style={{color:"red",textDecoration:"line-through"}}>
+        {key.work}
+        </span> :
+        <span style={{color:"blue", textDecoration:"none"}}>
+          {key.work}
+        </span>}
+        </td>
+      <td>
+      <button onClick={()=>{deletework(key.id)}}>Delete</button>
+      </td>
+      <td>
+        {abtnStatus ?
+        <button onClick={()=>{completework(key.id)}}>Complete</button>
+      
+      :
+        <button onClick={()=>{uncompletework(key.id)}}>unComplete</button>
+        }
+        </td>
+
+      <td>
+        <button onClick={()=>{editData(key.id,key.work)}}>Edit</button>
+      </td>
 
     </tr>
     </>
@@ -38,13 +85,17 @@ const ans=workData.map((key)=>{
         <h1>To Do App</h1>
         <hr size="1" color="blue" />
         Enter your Task : <input type="text" value={mywork} onChange={(e)=>{setMywork(e.target.value)}} />
-        <button onClick={handleSumit}>Add</button>
+        {btnStatus ?
+        <button onClick={handleSumit}>Add</button>:
+        <button onClick={editDataSave}>EditSave</button>}
         <hr />
-        <table>
+        <table border="1">
           <tr>
             <th>S.No</th>
             <th>Task</th>
-            <th>Actoion</th>
+            <th></th>
+            <th></th>
+            <th></th>
           </tr>
           {ans}
         </table>
--- src/todoSlice.jsx
+++ src/todoSlice.jsx
@@ -13,8 +13,51 @@ const todoSlice=createSlice({
         //  state.task.push(mydata)
         state.task.push(actions.payload);//B
      },
+     deleteTask:(state,actions)=>{
+        console.log(actions.payload)
+        // state.task=state.task.filter(item=>item.id!=actions.payload)
+        state.task=state.task.filter(function(key){
+            if(key.id==actions.payload)
+            {
+                return false
+            }
+            else
+            {
+                return true
+            }
+        })
+     },
+     completeTask:(state,actions)=>{
+     for(var i=0;i<state.task.length;i++)
+     {
+        if(state.task[i].id==actions.payload)
+        {
+            state.task[i].status=true
+        }
+     }
+     },
+      uncompleteTask:(state,actions)=>{
+        for(var i=0;i<state.task.length;i++)
+        {
+            if(state.task[i].id==actions.payload)
+            {
+                state.task[i].status=false
+            }
+        }
+
+      },
+      editTask:(state,actions)=>{
+        for(var i=0;i<state.task.length;i++)
+        {
+            if(state.task[i].id==actions.payload.myid)
+            {
+                state.task[i].work=actions.payload.mywork
+            }
+        }
+      }
+
 
     }
 })
-export const {addTask}=todoSlice.actions;
+export const {addTask,deleteTask,completeTask,uncompleteTask,editTask}=todoSlice.actions;
 export default todoSlice.reducer;
